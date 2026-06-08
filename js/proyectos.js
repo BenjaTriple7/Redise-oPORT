bc18d94aec0a181c634748fc8b0e77c98f6d106d
BenjaTriple7 <be.sanchezg@duocuc.cl>
2026-06-08 12:29:42 -0400

    jsonfiltros

--- js/proyectos.js
+++ js/proyectos.js
@@ -1,11 +1,94 @@
+let proyectosGlobal = [];
+
 async function cargarProyectos() {
 
     const response = await fetch('./data/proyectos.json');
+
     const proyectos = await response.json();
 
     proyectos.sort((a, b) => a.orden - b.orden);
 
+    proyectosGlobal = proyectos;
+
+    generarFiltros(proyectos);
+
     renderizarProyectos(proyectos);
+}
+
+cargarProyectos();
+
+function generarFiltros(proyectos) {
+
+    const filtersContainer = document.getElementById('filters');
+
+    const categorias = new Set();
+
+    proyectos.forEach(proyecto => {
+
+        proyecto.categorias.forEach(categoria => {
+            categorias.add(categoria);
+        });
+
+    });
+
+    filtersContainer.innerHTML = `
+    <button
+        class="btn btn-ghost filter-btn active"
+        data-filter="Todos"
+    >
+        Todos
+    </button>
+    `;
+
+    [...categorias].sort().forEach(categoria => {
+
+        filtersContainer.innerHTML += `
+            <button
+                class="btn btn-ghost filter-btn"
+                data-filter="${categoria}"
+            >
+                ${categoria}
+            </button>
+        `;
+
+    });
+
+    inicializarFiltros();
+}
+
+function inicializarFiltros() {
+
+    const botones = document.querySelectorAll('.filter-btn');
+
+    botones.forEach(boton => {
+
+        boton.addEventListener('click', () => {
+
+            botones.forEach(btn =>
+                btn.classList.remove('active')
+            );
+
+            boton.classList.add('active');
+
+            const filtro = boton.dataset.filter;
+
+            if (filtro === 'Todos') {
+
+                renderizarProyectos(proyectosGlobal);
+
+                return;
+            }
+
+            const filtrados = proyectosGlobal.filter(proyecto =>
+                proyecto.categorias.includes(filtro)
+            );
+
+            renderizarProyectos(filtrados);
+
+        });
+
+    });
+
 }
 
 function renderizarProyectos(proyectos) {
